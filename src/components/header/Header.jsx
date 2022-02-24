b930dda072ca47e06e52161d39b2f77fb05e1c1b
mrvalraven <akatiago@gmail.com>
2022-02-24 05:37:11 +0000

    added responsive styles to navbar and respective components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,95 +1,116 @@
-import React, { useEffect } from "react";
+import React, { useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 import Logo from "../../assets/logo.png";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 
 import "./styles.scss";
 
-const Header = () => {
+const Header = ({ appRef }) => {
+  const { width: windowWidth } = useWindowDimensions();
   let location = useLocation();
+  const burgerIconRef = useRef(null);
+  const burgerTextRef = useRef(null);
+  const navbarRef = useRef(null);
+
+  const toggleNavbarMenu = () => {
+    if (windowWidth < 1000) {
+      burgerIconRef.current.classList.toggle("activatedNavbarBurger");
+      burgerTextRef.current.classList.toggle("toggleBurgerText");
+      navbarRef.current.classList.toggle("toggleNavbar");
+      appRef.current.classList.toggle("removeScrolling");
+    }
+  };
 
   return (
     <header>
-      <Link to="/" className="logo">
+      <Link onClick={toggleNavbarMenu} to="/" className="logo">
         <img src={Logo} alt="logo" />
       </Link>
-      {window.innerWidth > 800 ? (
-        <nav>
-          <ul className="activatedNavbar">
-            <div className="logoMobile">
-              <Link to="/" className="logo">
-                <img src={Logo} alt="logo" />
-              </Link>
-              <hr />
-            </div>
-            <Link
-              to="/"
-              className={`navItem ${
-                location.pathname === "/" ? "currentPage" : ""
-              }`}
-            >
-              Home
+
+      <nav ref={navbarRef}>
+        <ul className="activatedNavbar">
+          <div className="logoMobile">
+            <Link onClick={toggleNavbarMenu} to="/" className="logo">
+              <img src={Logo} alt="logo" />
             </Link>
-            <Link
-              to="/calendario"
-              className={`navItem ${
-                location.pathname === "/calendario" ? "currentPage" : ""
-              }`}
-            >
-              Calendário
-            </Link>
-            <li className="dropdown">
-              <div className="text navItem">
-                <a className="dropdownName">Torneio</a>
-                <i className="fas fa-chevron-down"></i>
-              </div>
-              <div className="subMenu">
-                <Link
-                  to="/grupos"
-                  className={`navItem ${
-                    location.pathname === "/grupos" ? "currentPage" : ""
-                  }`}
-                >
-                  <span>⚆</span> Grupos
-                </Link>
-                <Link
-                  to="/leaderboard"
-                  className={`navItem ${
-                    location.pathname === "/leaderboard" ? "currentPage" : ""
-                  }`}
-                >
-                  <span>⚆</span> Jogadores
-                </Link>
-              </div>
-            </li>
-            <Link
-              to="/regulamento"
-              className={`navItem ${
-                location.pathname === "/regulamento" ? "currentPage" : ""
-              }`}
-            >
-              Regulamento
-            </Link>
-            <Link
-              to="/contactos"
-              className={`navItem ${
-                location.pathname === "/contactos" ? "currentPage" : ""
-              }`}
-            >
-              Contactos
-            </Link>
-          </ul>
-        </nav>
-      ) : (
-        <div className="burger">
-          <div className="burgerText">MENU</div>
-          <div className="drawing">
-            <div className="line1"></div>
-            <div className="line2"></div>
-            <div className="line3"></div>
+            <hr />
           </div>
+          <Link
+            onClick={toggleNavbarMenu}
+            to="/"
+            className={`navItem ${
+              location.pathname === "/" ? "currentPage" : ""
+            }`}
+          >
+            Home
+          </Link>
+          <Link
+            onClick={toggleNavbarMenu}
+            to="/calendario"
+            className={`navItem ${
+              location.pathname === "/calendario" ? "currentPage" : ""
+            }`}
+          >
+            Calendário
+          </Link>
+          <li className="dropdown">
+            <div className="text navItem">
+              <a className="dropdownName">Torneio</a>
+              <i className="fas fa-chevron-down"></i>
+            </div>
+            <div className="subMenu">
+              <Link
+                onClick={toggleNavbarMenu}
+                to="/grupos"
+                className={`navItem ${
+                  location.pathname === "/grupos" ? "currentPage" : ""
+                }`}
+              >
+                <span>⚆</span> Grupos
+              </Link>
+              <Link
+                onClick={toggleNavbarMenu}
+                to="/leaderboard"
+                className={`navItem ${
+                  location.pathname === "/leaderboard" ? "currentPage" : ""
+                }`}
+              >
+                <span>⚆</span> Jogadores
+              </Link>
+            </div>
+          </li>
+          <Link
+            onClick={toggleNavbarMenu}
+            to="/regulamento"
+            className={`navItem ${
+              location.pathname === "/regulamento" ? "currentPage" : ""
+            }`}
+          >
+            Regulamento
+          </Link>
+          <Link
+            onClick={toggleNavbarMenu}
+            to="/contactos"
+            className={`navItem ${
+              location.pathname === "/contactos" ? "currentPage" : ""
+            }`}
+          >
+            Contactos
+          </Link>
+        </ul>
+      </nav>
+
+      <div className="burger" onClick={toggleNavbarMenu}>
+        <div className="burgerText" ref={burgerTextRef}>
+          MENU
         </div>
-      )}
+        <div className="drawing" ref={burgerIconRef}>
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+      </div>
     </header>
   );
 };
